f2d6ec1b46eb1e1fcde79ec88b5b9f5440b46c9f
yasminab23-meet <109899977+yasminab23-meet@users.noreply.github.com>
2022-07-27 16:52:37 +0300

    FINAL Flask-Routing-Lab !!

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -18,6 +18,27 @@ def product_page():
     return render_template("product.html")
 
 
+@app.route ('/sweatshirt')
+def buy_sweatshirt():
+    return render_template('sweatshirt.html')
+
+@app.route('/table')
+def buy_table():
+    return render_template('table.html')
+
+@app.route('/bags')
+def buy_bags():
+    return render_template('bags.html')
+
+@app.route('/totebag')
+def buy_totebag():
+    return render_template('totebag.html')
+
+
+@app.route('/cart')
+def cart_page():
+    return render_template('cart.html')
+
 
 # Your code should be above
 
